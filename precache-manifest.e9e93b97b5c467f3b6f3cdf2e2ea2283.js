894b2bc2ff65e07a73db4e379d5ece82d27a436d
seifabouissa <seifeldinashrafabouissa@gmail.com>
2020-08-04 22:59:56 +0200

    Updates

---
+++ precache-manifest.e9e93b97b5c467f3b6f3cdf2e2ea2283.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e35d61c88f6cd90f44f8c8e74dc52f04",
+    "url": "/Reindeer-Landing-Page/index.html"
+  },
+  {
+    "revision": "421b56371e86c67e733c",
+    "url": "/Reindeer-Landing-Page/static/css/main.442aa717.chunk.css"
+  },
+  {
+    "revision": "cc8bf6f7375bb7f4e0f1",
+    "url": "/Reindeer-Landing-Page/static/js/2.37cc2f7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reindeer-Landing-Page/static/js/2.37cc2f7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "421b56371e86c67e733c",
+    "url": "/Reindeer-Landing-Page/static/js/main.73eb6589.chunk.js"
+  },
+  {
+    "revision": "b4d29b1cbd416709a590",
+    "url": "/Reindeer-Landing-Page/static/js/runtime-main.5bd43e1f.js"
+  },
+  {
+    "revision": "c159bf6dc53103189ace9fcfe8d631a6",
+    "url": "/Reindeer-Landing-Page/static/media/reindeer.c159bf6d.png"
+  }
+]);
